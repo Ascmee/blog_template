d1f535f59750fa301ef90ed1533d8a13c44a3b82
Acc <67445578+Ascmee@users.noreply.github.com>
2023-12-06 21:25:24 +0800

    Update file.js

--- js/file.js
+++ js/file.js
@@ -1,6 +1,2 @@
-var name_arr = [
-  { date: "2023/12/06/20:25:28", title: "C++Test", classification: "Other" },
-  { date: "2023/12/06/20:25:55", title: "C++Test", classification: "Other" },
-  { date: "2023/12/06/20:54:22", title: "C++学习", classification: "Other" },
-];
-var classification_arr = ["Java", "C++", "Asm", "Other"];
+var name_arr = [];
+var classification_arr = [];
